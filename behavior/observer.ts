2ad0519d3fc0b8b2c0681d49b44931793be27f5f
PaulKujawa <2843557+PaulKujawa@users.noreply.github.com>
2023-06-20 23:15:01 +0200

    Create observer.ts

---
+++ behavior/observer.ts
@@ -0,0 +1,74 @@
+class Observable {
+    #observers: Observer[] = [];
+
+    public addObserver(observer: Observer): void {
+        this.#observers.push(observer);
+    }
+
+    public deleteObserver(observer: Observer): void {
+        this.#observers.filter((obs) => obs !== observer);
+    }
+
+    protected notifyObservers(value: unknown): void {
+        this.#observers.forEach((observer) => observer.update(this, value));
+    }
+}
+
+interface Observer {
+    update(observable: Observable, value: unknown): void;
+}
+
+// =================================
+// = Concrete Implementation below =
+// =================================
+
+class Product extends Observable {
+    name: string;
+    #price: number;
+
+    constructor(name: string, price: number) {
+        super();
+        this.name = name;
+        this.#price = price;
+    }
+
+    raiseBid(bidPrice: number): void {
+        if (this.#price < bidPrice) {
+            console.log(`bid of ${this.name} was raised to ${bidPrice}.`);
+
+            this.#price = bidPrice;
+            this.notifyObservers(bidPrice);
+        }
+    }
+}
+
+class Bidder implements Observer {
+    #name: string;
+
+    constructor(name: string) {
+        this.#name = name;
+    }
+
+    // in theory the types can not be said for so sure, as Bidder could be
+    // passed to other Observables, too.
+    update(product: Product, value: number): void {
+        console.log(`${this.#name} was informed about the new price of ${value} for ${product.name}`);
+    }
+}
+
+// ========================
+// = Concrete Usage below =
+// ========================
+
+const bidderTom = new Bidder('Tom');
+const bidderLeo = new Bidder('Leo');
+
+const product1 = new Product('macbook', 200);
+product1.addObserver(bidderTom);
+
+const product2 = new Product('lamp', 20);
+product2.addObserver(bidderTom);
+product2.addObserver(bidderLeo);
+
+product1.raiseBid(250);
+product2.raiseBid(50);
